ee0dd16c4e34a24899ecee5783899ddc45e47133
Marat Galiev <kazanlug@gmail.com>
2017-07-10 17:56:26 +0300

    Bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ElixirUber.Mixfile do
   def project do
     [
       app: :elixir_uber,
-      version: "0.0.6",
+      version: "0.0.7",
       elixir: "~> 1.3",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
